9b843fef7864b7480b1fcc20f0d82cf6c96ff8c2
nothing <ddwi2504@gmail.com>
2025-02-27 23:16:41 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',  // Backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
   build: {
     outDir: 'dist'
   }
